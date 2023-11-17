556592531820d5c9321a48097cbc56e4765b2d20
Rose <rosebeats09@gmail.com>
2023-11-16 20:45:01 -0500

    Create form for getting user's location with search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "../src/style.css";
+
+const form = document.getElementById("location-form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const location = document.getElementById("location").value;
+    result.textContent = `${location}`;
+    form.reset();
+    console.log(location);
+});
