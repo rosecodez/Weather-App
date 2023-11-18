9476705eca6ef09235719e68c5ec0a369efce8de
Rose <rosebeats09@gmail.com>
2023-11-17 19:08:42 -0500

    Create display function for weather

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", (event) => {
 
 submit.addEventListener("click", () => {
     const location = document.getElementById("location").value;
-    fetch('https://api.weatherapi.com/v1/current.json?key=e8f61a33b1604c65bb3225134231411&q=' + location, {mode: 'cors'})
+    let data = fetch('https://api.weatherapi.com/v1/current.json?key=e8f61a33b1604c65bb3225134231411&q=' + location, {mode: 'cors'})
         .then(function(response) {
             return response.json();
         })
@@ -23,4 +23,26 @@ submit.addEventListener("click", () => {
         .catch(e => {
             console.log(e);
         });
+
 });
+function displayItems() {
+
+}
+function createLocation() {
+    let country = document.createElement("p");
+    let localTime = document.createElement("p");
+    let name = document.createElement("p");
+    let region = document.createElement("p");
+    let cloud = document.createElement("p");
+    let text = document.createElement("p");
+    let feelsLikeC = document.createElement("p");
+    let feelsLikeF = document.createElement("p");
+    let humidity = document.createElement("p");
+    let lastUpdated = document.createElement("p");
+    let temperatureC = document.createElement("p");
+    let temperatureF = document.createElement("p");
+    let windDirection = document.createElement("p");
+    let windKph = document.createElement("p");
+    let windMph = document.createElement("p");
+}
+createLocation();
