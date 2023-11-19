2874ea06309ba8b6cf7dbc131965bdb7f076ba4d
Rose <rosebeats09@gmail.com>
2023-11-18 23:31:04 -0500

    Arrange order of weather data

--- src/index.js
+++ src/index.js
@@ -9,21 +9,20 @@ form.addEventListener("submit", (event) => {
     result.textContent = `Searched: ${location}`;
     form.reset();
 });
-
 submit.addEventListener("click", () => {
     const location = document.getElementById("location").value;
-    const cloud = document.getElementById("cloud");
-    const country = document.getElementById("country");
-    const localTime = document.getElementById("location");
     const name = document.getElementById("name");
+    const localTime = document.getElementById("location");
     const region = document.getElementById("region");
-    const text = document.getElementById("text");
-    const feelsLikeC = document.getElementById("feelsLikeC");
-    const feelsLikeF = document.getElementById("feelsLikeF");
-    const humidity = document.getElementById("humidity");
+    const country = document.getElementById("country");
     const lastUpdated = document.getElementById("lastUpdated");
     const temperatureC = document.getElementById("temperatureC");
     const temperatureF = document.getElementById("temperatureF");
+    const text = document.getElementById("text");
+    const feelsLikeC = document.getElementById("feelsLikeC");
+    const feelsLikeF = document.getElementById("feelsLikeF");
+    const cloud = document.getElementById("cloud");
+    const humidity = document.getElementById("humidity");
     const windKph = document.getElementById("windKph");
     const windMph = document.getElementById("windMph");
     fetch('https://api.weatherapi.com/v1/current.json?key=e8f61a33b1604c65bb3225134231411&q=' + location, {mode: 'cors'})
@@ -32,19 +31,20 @@ submit.addEventListener("click", () => {
         })
         .then(function(response) {
             console.log(response);
-            cloud.textContent = "Cloud: " + response.current.cloud;
-            localTime.textContent = "Local time: " + response.location.localtime;
+            
             name.textContent = "City: " + response.location.name;
+            localTime.textContent = "Local time: " + response.location.localtime;
             region.textContent = "Region: " + response.location.region;
+            country.textContent = "Country: " + response.location.country;
+            temperatureC.textContent = "Temperature (C): " + response.current.temp_c;
+            temperatureF.textContent = "Temperature (F): " + response.current.temp_f;
             text.textContent = "Weather: " + response.current.condition.text;
             feelsLikeC.textContent = "Feels like (C): " + response.current.feelslike_c;
             feelsLikeF.textContent = "Feels like (F): " + response.current.feelslike_f;
+            cloud.textContent = "Cloud: " + response.current.cloud;
             humidity.textContent = "Humidity: " + response.current.humidity;
             lastUpdated.textContent = "Last updated: " + response.current.last_updated;
-            temperatureC.textContent = "Temperature (C): " + response.current.temp_c;
-            temperatureF.textContent = "Temperature (F): " + response.current.temp_f;
             windKph.textContent = "Wind (Kph): " + response.current.wind_kph;
             windMph.textContent = "Wind (Mph): " + response.current.wind_mph;
-            country.textContent = "Country: " + response.current.cloud;
         })
 });
